a6bf601967ca17eb6ddf1625a2ee5a3464d23c57
technowizard <aheisleycook@gmail.com>
2016-06-21 09:28:34 -0700

    Update testspeech.py

--- testspeech.py
+++ testspeech.py
@@ -4,7 +4,6 @@ import os
 # obtain audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone() as source:
-    os.system("say please speak what you would like to be writen")
     audio = r.listen(source)
 text = r.recognize_sphinx(audio)
 os.system("cd transcriptions")
