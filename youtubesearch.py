22c51eee3a83b7d9ac85595fdf474173aa0f6b39
technowizard <aheisleycook@gmail.com>
2016-08-29 17:55:28 -0700

    Update youtubesearch.py

--- youtubesearch.py
+++ youtubesearch.py
@@ -1,6 +1,6 @@
 import webbrowser
 videosearch = input("type what you would like to search")
-webbrowser.open("http://www.youtube.com/results?search_query=" +  videosearch)
+webbrowser.open("http://www.youtube.com/results?search_query=" +  videosearch + "&page=1")
 
 
  
