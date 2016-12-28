056cb82280d4c77515beb8bea6a11b699356f3ef
technowizard <aheisleycook@gmail.com>
2016-12-27 21:01:45 -0700

    Update googlethat.py

--- googlethat.py
+++ googlethat.py
@@ -1,7 +1,7 @@
 import webbrowser
-usersearch = input("what do you want to search")
+usersearch = input("type in what you would like to search")
 webbrowser.open("http://www.google.com/search?q=" + usersearch + "&page=1")
-
+print("search is complete")
 
 
  
