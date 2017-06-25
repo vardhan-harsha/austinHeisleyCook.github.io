45f9dba65252adbaf609ad2c557f4b019ca75d59
technowizard <aheisleycook@gmail.com>
2017-06-24 17:21:15 -0700

    calculator app

--- calculator.py
+++ calculator.py
@@ -1,60 +1,92 @@
-import sys
-import os
-import winsound
-import time
-counter = 1
+from tkinter import *
+from fractions import *
+from easygui import *
+class app():
+        
+        def __init__(self, master):
+             frame = Frame(master, width=1000, height=1000)
+             frame.pack()
+
+             self.number = 0
+             root.title("my app")
+             self.apptitle = Label(frame, text="my calculator".title(), border=3)
+             self.apptitle.pack(side="top")
+             """buttons to control calculator functions"""
+             self.add = Button(frame, text="+", fg="red", border=1, command=self.add)
+             self.add.pack(fill="both", pady = 10, padx= 30)
+             self.sub = Button(frame, text="-", fg="red", border=1, command=self.subt)
+             self.sub.pack(fill="both", pady = 10, padx= 30)
+             self.mult = Button(frame, text="X", fg="red", border=1, command=self.mult)
+             self.mult.pack(fill="both", pady = 10, padx= 30)
+             self.div = Button(frame, text="/", fg="red", border=1, command=self.div)
+             self.div.pack(fill="both", pady = 10, padx= 30)
+             self.expo = Button(frame, text="x^Y", fg="red", border=1, command=self.ex)
+             self.expo.pack(fill="both", pady = 10, padx= 30)
+             self.check = Button(frame, text="x\n_\ny", fg="red", border=1, command=self.fractions)
+             self.check.pack(fill="both", pady = 10, padx= 30)
+             self.button3 = Button(frame, text="quit", fg="purple", border=4, command=frame.quit)
+             self.button3.pack(fill="both", pady = 10, padx= 30)
+             #numbers calculate
+             self.x = StringVar(str(""))
+             self.y = StringVar(str(""))
+             self.r = StringVar(str(""))
+             self.hc = StringVar(str(""))
+             #first int and second
+             self.int1 = Entry(frame, textvariable=self.x)
+             self.int1.pack(side="top")
+             self.int2 = Entry(frame, textvariable=self.y)
+             self.int2.pack(side="top")
+             #result calculation
+             self.int3 = Entry(frame, textvariable=self.r)
+             self.int3.pack(side="top")
+            
+        """basic math operation functions"""
+        
+        def subt(self):
+             self.x2 = str(self.x.get())
+             self.x3 = str(self.y.get())
+             self.r3 = int(self.x2) -  int(self.x3)  
+             if self.x2 == "" or self.x3 == "":
+                 self.v = self.r.set("can't compute due to blank entry")
+             else:
+                 self.v = self.r.set(str(self.r3))
+        def add(self):
+             self.x2 = str(self.x.get())
+             self.x3 = str(self.y.get())
+             self.r3 = int(self.x2) +  int(self.x3)
+             if self.x2 == "" or self.x3 == "":
+                 self.v = self.r.set("can't compute due to blank entry")
+             else:
+                self.v = self.r.set(str(self.r3))     
+        
+        def mult(self):
+             self.x2 = str(self.x.get())
+             self.x3 = str(self.y.get())
+             self.r3 = int(self.x2) *  int(self.x3)  
+             if self.x2 == "" or self.x3 == "":
+                 self.v = self.r.set("can't compute due to blank entry")
+             else: 
+                self.v = self.r.set(str(self.r3))     
+        def div(self):
+             self.x2 = str(self.x.get())
+             self.x3 = str(self.y.get())
+             self.r3 = int(self.x2) /  int(self.x3)  
+             self.v = self.r.set(str(self.r3))     
+        
+        def ex(self):
+             self.x2 = str(self.x.get())
+             self.x3 = str(self.y.get())
+             self.r3 = int(self.x2) **  int(self.x3)  
+             self.v = self.r.set(str(self.r3))  
+        def fractions(self):
+             self.x2 = str(self.x.get())
+             self.x3 = str(self.y.get())
+             self.r3 =  Fraction(int(self.x2), int(self.x3))  
+             self.v = self.r.set(self.r3 ) 
+             
+
+root = Tk()
 
 
-while True:
- os.system("title my calculator && color 1b ")	
- x = int(input("first integer:"))
- y = int(input("second integer:"))
- operator = input("operation:") 
- if operator == "+":
-  print("\tresult:")	 
-  print(x + y)
-  counter=counter-1
-  restart = input("do you want to try another calculation? type yes/no")
-  if restart == "no":
-   sys.exit()	
-  if restart == "yes":
-   counter = 1 
- if operator == "-":
-  print("result:")	 
-  print(x - y)
-  counter=counter-1
-  restart = input("do you want to try another calculation? type yes/no")
-  if restart == "no":
-   sys.exit()	
-  if restart == "yes":
-   counter = 1 
- if operator == "/":
-  print("result:")	 
-  print(x / y)
-  counter=counter-1
-  restart = input("do you want to try another calculation? type yes/no")
-  if restart == "no":
-   sys.exit()	
-  if restart == "yes":
-   counter = 1 	
- if operator == "*":
-  print("result:")	 
-  print(x * y)	  	
-  counter=counter-1
-  restart = input("do you want to try another calculation? type yes/no")
-  if restart == "no":
-   print("created by technowizard")	  
-   time.sleep(4)	  
-   sys.exit()	
-  if restart == "yes":
-   counter = 1
-  if restart != "no" or restart != "yes" or restart == "":
-   winsound.Beep(90,90)  
-   os.system("color a3")
-   restart = input(restart +" is not a valid answer do you want to try another calculation? type yes/no")
-    	  
-   
- 
- 		
-
-
+App = app(root)
+root.mainloop()
