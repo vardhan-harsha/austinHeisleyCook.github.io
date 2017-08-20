f54a94cb704e39762d579560cf2b20f942df6b0c
technowizard <aheisleycook@gmail.com>
2017-08-20 15:57:16 -0700

    argument update

--- googlethat.py
+++ googlethat.py
@@ -1,5 +1,7 @@
 import webbrowser
-usersearch = input("type in what you would like to search")
+import sys
+
+usersearch = sys.argv[1]
 webbrowser.open("http://www.google.com/search?q=" + usersearch + "&page=1")
 print("search is complete")
 
